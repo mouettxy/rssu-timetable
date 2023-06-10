653539a57deb31095c0dbf1e50ed59df67532370
Никита Чайковский <lis@chaikovskie.com>
2023-06-11 00:08:27 +0300

    feat: production url

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://127.0.0.1:8090/",
+  baseURL: "http://rssutimetable.ru/api",
   timeout: 3000,
   headers: {
     "Content-Type": "application/json",
